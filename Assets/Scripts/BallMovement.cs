b19f8bd1b4957d7d11f5ab2aab037fc37f967658
SongbirdZero <alanah.ohalloran@students.mq.edu.au>
2025-04-29 11:39:55 +1000

    Added ball prefab
    Some scripts implemented

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    //private Vector3 speed = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Bat")) {
+            //speed = 
+        }
+    }
+}
