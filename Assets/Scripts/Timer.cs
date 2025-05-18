11b67877f277dac76aa0c01728762af7b3948469
sevak777 <69232210+sevak777@users.noreply.github.com>
2025-05-19 00:23:36 +1000

    Added Timer on this branch

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 180f; 
+    public Text timerText;
+    private bool timerIsRunning = true;
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                UpdateTimerDisplay(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                EndScene(); 
+            }
+        }
+    }
+
+    void UpdateTimerDisplay(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void EndScene()
+    {
+         SceneManager.LoadScene("VRScene"); // VRScene being used as a place holder till game ending UI scene is created
+    }
+}
+
