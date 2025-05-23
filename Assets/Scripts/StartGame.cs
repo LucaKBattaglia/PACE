90041c8cfb2d6738327f5b64ca5ad5f187cdb5e0
sevak777 <69232210+sevak777@users.noreply.github.com>
2025-05-23 23:02:36 +1000

    UI testing

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public string sceneToLoad = "Main"; 
+    
+     public void StartTheGame()
+    {
+        {
+            Debug.Log("Button Clicked");
+             SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
