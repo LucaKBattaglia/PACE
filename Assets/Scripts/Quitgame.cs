792bc528cf5b2120a03a6b3179689e25b4297de9
sevak777 <69232210+sevak777@users.noreply.github.com>
2025-05-24 18:37:52 +1000

    Added script Quitgame
    added a script to close game for menu and pause menu

---
+++ Assets/Scripts/Quitgame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{
+    public Button quitButton;
+
+    void Start()
+    {
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
